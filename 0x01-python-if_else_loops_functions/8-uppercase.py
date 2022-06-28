226478b3f25389446fdffa8cf2028d90fd523d76
Cyril <cyril.idu2@gmail.com>
2022-06-28 16:01:37 +0100

    getting used to it

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            c = chr(ord(c) - (ord('a') - ord('A')))
+        print("{:s}".format(c), end='')
+    print("")
