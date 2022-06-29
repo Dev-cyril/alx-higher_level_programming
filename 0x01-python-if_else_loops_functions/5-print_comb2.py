36fd6118a59db82b452548bbb874ec8ce9143b46
cyril <cyril.idu2@gmail.com>
2022-06-28 18:59:01 -0700

    combo2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numb in range(0, 100):
     if numb == 99:
-        print(numb)
+        print("{:d}".format(numb))
     else:
-        print(str(numb).zfill(2), end=', ')
+        print("{}".format(str(numb).zfill(2)), end=', ')
