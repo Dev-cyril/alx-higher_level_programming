1f4553e606e2e4583b476a09b3d912199791ed2d
cyril <cyril.idu2@gmail.com>
2022-07-05 11:53:26 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     length = len(my_string)
     for i in range(0, length):
         new_string = my_string.translate({ord(c): None for c in "cC"})
