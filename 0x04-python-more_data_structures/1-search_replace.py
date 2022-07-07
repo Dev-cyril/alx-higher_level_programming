c5f34866026f2dd8d50e515e2f3b197330442972
Cyril Idu <cyril.idu2@gmail.com>
2022-07-07 02:33:42 +0100

    Others

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = (list(map((replace if search in my_list else search), i)) for i in my_list)
-    return my_list
-
-ls = [1,2,3,45,5,6,7,8,9,12]
-nw = search_replace(ls, 2, 89)
-print(nw)
-# print(ls)
+    def s_r_elm(elm):
+        return (elm if elm != search else replace)
+    return list(map(s_r_elm, my_list))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-# import 
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
-        return 0
+    ns = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if ns[roman_string[i]] >= ns[roman_string[i + 1]]:
+                total += ns[roman_string[i]]
+            else:
+                total -= ns[roman_string[i]]
+            i += 1
+        total += ns[roman_string[i]]
+        return total
     else:
-        roman_string = roman_string.upper()
-        if (roman_string == 'I'):
-            return 1
-        if (roman_string == 'V'):
-            return 5
-        if (roman_string == 'X'):
-            return 10
-        if (roman_string == 'C'):
-            return 100
-        if (roman_string == 'L'):
-            return 50
-        if (roman_string == 'D'):
-            return 500
-        if (roman_string == 'M'):
-            return 1000
-        
+        return 0
