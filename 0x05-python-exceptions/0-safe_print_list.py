792ce5a3a27cbe9db6a054d181990763b79dd1a4
cyril <cyril.idu2@gmail.com>
2022-07-19 16:42:32 -0700

    God abeg

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     length = 0
     for i in range(x):
         try:
-            length += 1
             print(my_list[i], end='')
+            length += 1
         except IndexError:
             break
     print()
