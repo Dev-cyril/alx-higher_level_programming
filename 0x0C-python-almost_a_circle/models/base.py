9bde58388845b2c9d05f52b1c971e4049e111981
cyril <cyril.idu2@gmail.com>
2022-08-17 17:47:10 -0700

    updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,7 +45,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns the list of json objects"""
-        if json_string is None:
+        if json_string is None or len(json_string) == 0:
             return []
         else:
             return json.loads(json_string)
