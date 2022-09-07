e01147845816067092e2c10ef29de1f3bde8014e
cyril <cyril.idu2@gmail.com>
2022-09-07 07:01:47 -0700

    task #1 and task #2

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- A script that creates a database if it doesn't exist
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- A script that deletes a database if it exist
+
+DELETE DATABASE IF EXISTS hbtn_0c_0;
