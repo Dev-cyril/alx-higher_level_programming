7ea0c7f96b0a5c3a4786128329e026ecc2ae9543
Cyril <cyril.idu2@gmail.com>
2022-09-08 13:11:08 +0100

    task #12 to #16

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- a script that updates data from a table
+
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- a script that deletes records with score <= 5
+
+DELETE FROM second_table WHERE score <= 5;
