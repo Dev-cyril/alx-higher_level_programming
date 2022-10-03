13135a1b010b04ddfce67b23412188af66c0c793
Cyril <cyril.idu2@gmail.com>
2022-10-03 21:04:12 +0000

    sorting array of integers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const arr = [];
 for (const i in process.argv) {
   arr.push(process.argv[i] * 1);
 }
-arr.sort(function  (a, b) {
+arr.sort(function (a, b) {
   return b - a;
 });
 console.log((arr.length > 3) ? arr[1] : 0);
