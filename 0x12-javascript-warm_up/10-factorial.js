d3ccfcfa552580d90a3325ef1e54b286f31f8459
Cyril <cyril.idu2@gmail.com>
2022-10-03 16:16:25 +0000

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const argument = process.argv[2] * 1;
+function factorial (fac) {
+  return((isNaN(fac)) || (fac === 1) ? 1 : (fac * factorial(fac - 1)));
+}
+console.log(factorial(argument));
