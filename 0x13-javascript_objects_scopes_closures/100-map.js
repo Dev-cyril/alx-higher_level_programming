b58fa8b059e5e7728a5824edcff2c69fa9107818
Cyril <cyril.idu2@gmail.com>
2022-10-05 00:40:18 +0100

    chnages for semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,7 +3,5 @@
 const arr = require('./100-data').list;
 let newArr = [];
 console.log(arr);
-for (let i in arr) {
-  newArr.map(arr[i] * i)
-}
+newArr.map((x, index) => x *index);
 console.log(newArr);
