bfd55e00c38e75bde7f0cf1db379292c7bfe4790
Cyril <cyril.idu2@gmail.com>
2022-10-07 14:16:03 +0100

    adding readme, task 0 - 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A module that list all data in `state` table in `hbtn_0e_0_usa` database
+    where name equals a search keyword fro the CLI
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    dataBaseConnect = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf-8')
+    data = dataBaseConnect.cursor()
+    data.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(str(argv[4])))
+    for eachRow in data:
+        print(eachRow)
+    data.close()
+    dataBaseConnect.close()
