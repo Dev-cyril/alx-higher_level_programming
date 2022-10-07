71df6c525a29e3063d43e98fb3542fac0de8fd8e
Cyril <cyril.idu2@gmail.com>
2022-10-07 23:40:52 +0100

    adding task 4-12

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a script that lists all State objects from the database hbtn_0e_6_usa
+    that has a charater of 'a' SQL injection free
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    for state in Session.query(State).order_by(State.id).all():
+        if argv[4] == state.name:
+            print(state.id)
+        else:
+            print("Not found")
+    Session.close()
---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a script that lists all City objects from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import Base, City
+from model_state import State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    for city, state in Session.query(City, State).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    Session.close()
