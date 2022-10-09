e23507e1caa7b750ef3e28c4648abe969f85294c
Cyril <cyril.idu2@gmail.com>
2022-10-09 05:14:50 +0100

    updates to 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -17,4 +17,4 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state')
+    cities = relationship('City', backref='state')
