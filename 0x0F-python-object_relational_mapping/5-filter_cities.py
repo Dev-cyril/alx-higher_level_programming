2ab0270722041817fba33eee1d561f291cf8772c
Cyril <cyril.idu2@gmail.com>
2022-10-10 04:59:16 +0100

    updates to 3 and 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,11 +14,10 @@ if __name__ == '__main__':
                                       passwd=argv[2], db=argv[3],
                                       charset='utf8')
     data = dataBaseConnect.cursor()
-    data.execute("SELECT name FROM cities WHERE state_id = \
-                (SELECT id FROM states WHERE name = {}) ORDER BY id ASC"
-                 .format(argv[4]))
-    dataTable = data.fetchall()
-    for eachRow in dataTable:
-        print(eachRow)
+    data.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = data.fetchall()
+    print(", ".join(city[0] for city in rows))
     data.close()
     dataBaseConnect.close()
